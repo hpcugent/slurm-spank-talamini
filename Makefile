c4d99c993626bba9e0800974a17feac33499c5f8
Andy Georges <itkovian@ugent.be>
2018-04-16 13:14:06 +0200

    Initial checkin

---
+++ Makefile
@@ -0,0 +1,8 @@
+all: private-tmpdir.so
+
+private-tmpdir.so: private-tmpdir.c
+	gcc -std=gnu99 -Wall -o env-test.o -fPIC -c env-test.c
+	gcc -shared -o env-test.so env-test.o
+
+clean:
+	rm -f env-test.o env-test.so
---
+++ bdist_rpm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VERSION=$(grep "Version:.*[0-9]" slurm-env-test.spec | tr -s " " |  awk '{print $2;}')
+RELEASE=$(grep "%global rel.*[-1-9]" slurm-env-test.spec | tr -s " " | awk '{print $3}')
+
+if [ "${RELEASE}" -gt 1 ]; then
+    SUFFIX=${VERSION}-${RELEASE}
+else
+    SUFFIX=${VERSION}
+fi
+
+GITTAG=$(git log --format=%ct.%h -1)
+
+mkdir -p BUILD SOURCES SPECS RPMS BUILDROOT
+git archive --format=tar.gz -o "SOURCES/slurm-env-test-${SUFFIX}.tar.gz" --prefix="slurm-env-test-${SUFFIX}/" HEAD
+cp slurm-env-test.spec "SPECS"
+rpmbuild --define "gittag ${GITTAG}" --define "_topdir $PWD" -ba SPECS/slurm-env-test.spec
