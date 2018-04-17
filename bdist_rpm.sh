c4b11f342dcf5c6774b9d188136a26a6db5475cd
Andy Georges <itkovian@gmail.com>
2018-04-17 16:36:09 +0200

    Moar cleanup

--- bdist_rpm.sh
+++ bdist_rpm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-VERSION=$(grep "Version:.*[0-9]" slurm-env-test.spec | tr -s " " |  awk '{print $2;}')
-RELEASE=$(grep "%global rel.*[-1-9]" slurm-env-test.spec | tr -s " " | awk '{print $3}')
+VERSION=$(grep "Version:.*[0-9]" slurm-spank-talamini.spec | tr -s " " |  awk '{print $2;}')
+RELEASE=$(grep "%global rel.*[-1-9]" slurm-spank-talamini.spec | tr -s " " | awk '{print $3}')
 
 if [ "${RELEASE}" -gt 1 ]; then
     SUFFIX=${VERSION}-${RELEASE}
@@ -12,6 +12,6 @@ fi
 GITTAG=$(git log --format=%ct.%h -1)
 
 mkdir -p BUILD SOURCES SPECS RPMS BUILDROOT
-git archive --format=tar.gz -o "SOURCES/slurm-env-test-${SUFFIX}.tar.gz" --prefix="slurm-env-test-${SUFFIX}/" HEAD
-cp slurm-env-test.spec "SPECS"
-rpmbuild --define "gittag ${GITTAG}" --define "_topdir $PWD" -ba SPECS/slurm-env-test.spec
+git archive --format=tar.gz -o "SOURCES/slurm-spank-talamini-${SUFFIX}.tar.gz" --prefix="slurm-spank-talamini-${SUFFIX}/" HEAD
+cp slurm-spank-talamini.spec "SPECS"
+rpmbuild --define "gittag ${GITTAG}" --define "_topdir $PWD" -ba SPECS/slurm-spank-talamini.spec
