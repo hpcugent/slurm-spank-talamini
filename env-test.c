4303b6c8990c8565575c5da609a26fa694245f64
Andy Georges <itkovian@gmail.com>
2018-04-17 09:27:36 +0200

    Debug output cleanup

--- env-test.c
+++ env-test.c
@@ -6,7 +6,7 @@
  * Author: Andy Georges
  *
  * Loosely based on Spank plugin private-tmpdir (c) HPC2N.umu.se
- * 
+ *
  */
 
 /* Needs to be defined before first invocation of features.h so enable
@@ -32,24 +32,21 @@ SPANK_PLUGIN(env-test, 1);
  */
 int slurm_spank_init(spank_t sp, int ac, char **av)
 {
-    slurm_info("Spank env-test slurm_spank_init");
+    char **env = environ;
+
+    slurm_debug("SPANK [env-test]: slurm_spank_init");
+
     if (spank_context() == S_CTX_LOCAL) {
-        char **env = environ;
-    	slurm_info("Spank env-test executing in local context");
-        while (*env != NULL) {
-            slurm_info("environment var found in spank env-test: %s", *env);
-            env++;
-        }
+    	slurm_debug("SPANK [env-test]: executing in local context");
     } else if (spank_context() == S_CTX_REMOTE) {
-    	slurm_info("Spank env-test executing in remote context");
-        char **env = environ;
-    	slurm_info("Spank env-test executing in local context");
-        while (*env != NULL) {
-            slurm_info("environment var found in spank env-test: %s", *env);
-            env++;
-        }
+    	slurm_debug("SPANK [env-test]: executing in remote context");
     }
- 
+
+    while (*env != NULL) {
+        slurm_debug2("SPANK [env-test]: %s", *env);
+        env++;
+    }
+
     return ESPANK_SUCCESS;
 }
 
@@ -57,3 +54,21 @@ int slurm_spank_exit(spank_t sp, int ac, char **av) {
     return ESPANK_SUCCESS;
 }
 
+
+int slurm_spank_job_prolog(spank_t sp, int ac, char**av) {
+    char **env = environ;
+
+    slurm_debug("SPANK [env-test]: slurm_spank_job_prolog");
+
+    if (spank_context() == S_CTX_LOCAL) {
+    	slurm_debug("SPANK [env-test]: executing in local context");
+    } else if (spank_context() == S_CTX_REMOTE) {
+    	slurm_debug("SPANK [env-test]: executing in remote context");
+    }
+
+    while (*env != NULL) {
+        slurm_debug2("SPANK [env-test]: env var: %s", *env);
+        env++;
+    }
+
+}
