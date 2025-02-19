6463113b37b31a06e51572bb45f3eb235f43902c
Andy Georges <itkovian@gmail.com>
2025-02-19 11:00:39 +0100

    fix: add header to python script

--- generate_pbs_nodefile.py
+++ generate_pbs_nodefile.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
-
 #
+# Copyright 2012-2025 Ghent University
+#
+# This file is part of slurm-spank-talamini,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://www.vscentrum.be),
+# the Flemish Research Foundation (FWO) (http://www.fwo.be/en)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# https://github.com/hpcugent/slurm-spank-talamini
+#
+# vsc-utils is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Library General Public License as
+# published by the Free Software Foundation, either version 2 of
+# the License, or (at your option) any later version.
+#
+# slurm-spank-talamini is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public License
+# along with vsc-utils. If not, see <http://www.gnu.org/licenses/>.
+#
+
+
 # Script to generate the pbs nodefile based on the
 # environment variables set by Slurm:
 # - SLURM_JOB_CPUS_PER_NODE
